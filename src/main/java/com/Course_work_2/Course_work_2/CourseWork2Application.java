1b39cb8f3f7952f52779c036cca1d9d279984c9c
sakeev92 <vs.sakeev@mail.ru>
2023-06-12 15:02:40 +0300

    HW

---
+++ src/main/java/com/Course_work_2/Course_work_2/CourseWork2Application.java
@@ -0,0 +1,13 @@
+package com.Course_work_2.Course_work_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Course_work_2/Course_work_2/CourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Course_work_2.Course_work_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
